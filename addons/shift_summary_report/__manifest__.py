217a8a1ddebdc09d6799ea5d29cfc5c2e26b5911
Семён Селиванов <semaselivan@gmail.com>
2026-08-03 19:07:42 +0300

    init shift_summary_report module structure and manifest

---
+++ addons/shift_summary_report/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Shift Summary Report for Director',
+    'version': '19.0.1.0.0',
+    'category': 'Manufacturing',
+    'summary': 'Automatic shift summary report generation and director notification',
+    'depends': [
+        'mrp',
+        'mail',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/mail_template_data.xml',
+        'views/shift_summary_report_views.xml',
+    ],
+    'demo': [
+        'data/shift_summary_report_demo.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
